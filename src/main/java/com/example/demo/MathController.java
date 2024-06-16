3a942cb63f3e9400c46ec328e9f4ca89d1258ac0
viktor-lima <joaolimak10@outlook.com>
2024-06-16 17:50:23 -0300

    variables not using have been removed

--- src/main/java/com/example/demo/MathController.java
+++ src/main/java/com/example/demo/MathController.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MathController {
-
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/sum/{numberOne}/{numberTwo}")
 	public Double greeting(
